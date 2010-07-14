bde6e70828da8413c6c9a2eca1d2c9db41f5b996
Matthias Kestenholz <mk@spinlock.ch>
2010-07-14 15:13:00 +0200

    Generate similar admin actions in factory function

--- elephantblog/models.py
+++ elephantblog/models.py
@@ -1,18 +1,20 @@
 from datetime import datetime
 
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.core.validators import ValidationError
 from django.db import models
 from django.db.models import signals, Q
-from django.utils.translation import ugettext_lazy as _, ugettext, get_language
-from django.conf import settings
+from django.template.defaultfilters import slugify
+from django.utils.translation import ugettext_lazy as _, ugettext, \
+    get_language, ungettext
+
 from feincms.admin import editor
 from feincms.management.checker import check_database_schema
 from feincms.models import Base
-from django.core.validators import ValidationError
 from feincms.translations import TranslatedObjectMixin, Translation, \
     TranslatedObjectManager
-from django.template.defaultfilters import slugify
 from feincms.content.application.models import reverse
 
 
@@ -241,6 +243,17 @@ class Entry(Base):
 signals.post_syncdb.connect(check_database_schema(Entry, __name__), weak=False)
 
 
+def entry_admin_update_fn(new_state, new_state_dict):
+    def _fn(self, request, queryset):
+        rows_updated = queryset.update(**new_state)
+
+        self.message_user(request, ungettext(
+            _('One entry was successfully marked as %(state)s'),
+            _('%(count)s entries were successfully marked as %(state)s'),
+            rows_updated) % {'state': new_state, 'count': rows_updated})
+    return _fn
+
+
 class EntryAdmin(editor.ItemEditor):
     date_hierarchy = 'published_on'
     list_display = ['__unicode__', 'published', 'last_changed', 'isactive', 'active_status', 'published_on', 'user', 'pinging']
@@ -253,62 +266,26 @@ class EntryAdmin(editor.ItemEditor):
     show_on_top = ['title', 'published', 'categories']
     raw_id_fields = []
 
-    def ping_again(self, request, queryset):
-        rows_updated = queryset.update(pinging=Entry.QUEUED)
-        if rows_updated == 1:
-            message_bit = _("1 entry was")
-        else:
-            message_bit = _("%s entries were") % rows_updated
-        self.message_user(request, _("%s successfully marked as queued.") % message_bit)
+
+    ping_again = entry_admin_update_fn(_('queued'), {'pinging': Entry.QUEUED})
     ping_again.short_description = _('ping again')
 
-    def mark_publish(self, request, queryset):
-        rows_updated = queryset.update(published=Entry.CLEARED)
-        if rows_updated == 1:
-            message_bit = _("1 entry was")
-        else:
-            message_bit = _("%s entries were") % rows_updated
-        self.message_user(request, _("%s successfully marked as cleared.") % message_bit)
+    mark_publish = entry_admin_update_fn(_('cleared'), {'published': Entry.CLEARED})
     mark_publish.short_description = _('mark publish')
 
-    def mark_frontpage(self, request, queryset):
-        rows_updated = queryset.update(published=Entry.FRONT_PAGE)
-        if rows_updated == 1:
-            message_bit = _("1 entry was")
-        else:
-            message_bit = _("%s entries were") % rows_updated
-        self.message_user(request, _("%s successfully marked as front-page.") % message_bit)
+    mark_frontpage = entry_admin_update_fn(_('front-page'), {'published': Entry.FRONT_PAGE})
     mark_frontpage.short_description = _('mark frontpage')
 
-    def mark_needs_reediting(self, request, queryset):
-        rows_updated = queryset.update(published=Entry.NEEDS_REEDITING)
-        if rows_updated == 1:
-            message_bit = _("1 entry was")
-        else:
-            message_bit = _("%s entries were") % rows_updated
-        self.message_user(request, _("%s successfully marked as need re-editing.") % message_bit)
+    mark_needs_reediting = entry_admin_update_fn(_('need re-editing'), {'published': Entry.NEEDS_REEDITING})
     mark_needs_reediting.short_description = _('mark re-edit')
 
-    def mark_inactive(self, request, queryset):
-        rows_updated = queryset.update(published=Entry.INACTIVE)
-        if rows_updated == 1:
-            message_bit = _("1 entry was")
-        else:
-            message_bit = _("%s entries were") % rows_updated
-        self.message_user(request, _("%s successfully marked as inactive.") % message_bit)
+    mark_inactive = entry_admin_update_fn(_('inactive'), {'published': Entry.INACTIVE})
     mark_inactive.short_description = _('mark inactive')
 
-    def mark_delete(self, request, queryset):
-        rows_updated = queryset.update(published=Entry.DELETED)
-        if rows_updated == 1:
-            message_bit = _("1 entry was")
-        else:
-            message_bit = _("%s entries were") % rows_updated
-        self.message_user(request, _("%s successfully marked as deleted.") % message_bit)
+    mark_delete = entry_admin_update_fn(_('deleted'), {'published': Entry.DELETED})
     mark_delete.short_description = _('remove')
 
     actions = (mark_publish, mark_frontpage, mark_needs_reediting, mark_inactive, mark_delete, ping_again)
-
 
     def save_model(self, request, obj, form, change):
         obj.user = request.user
@@ -318,4 +295,3 @@ class EntryAdmin(editor.ItemEditor):
 Entry.register_regions(
                 ('main', _('Main content area')),
                 )
-
