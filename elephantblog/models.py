07f9ec65658d8ed320ce506e529b3fb9db2cb051
Simon Schmid <ssc@feinheit.ch>
2010-08-17 17:27:28 +0200

    permalink for blogentrys feincms appcontent compatible

--- elephantblog/models.py
+++ elephantblog/models.py
@@ -207,9 +207,7 @@ class Entry(Base):
                       'month': "%02d" %self.published_on.month,
                       'day': "%02d" %self.published_on.day,
                       'slug': self.slug}
-                      # TODO support application content integration:
-        #return reverse('elephantblog.urls/elephantblog.views.entry', kwargs=entry_dict)
-        return reverse('elephantblog.views.entry', kwargs=entry_dict)
+        return reverse('elephantblog.urls/elephantblog.views.entry', kwargs=entry_dict)
 
     @classmethod
     def register_extension(cls, register_fn):
