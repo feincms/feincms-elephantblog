3e34f5b60e67e64cda8ccdab60938aefbeabe2ae
Jamie Matthews <jamie.matthews@gmail.com>
2011-04-13 13:24:22 +0100

    Add a simple setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='feincms-elephantblog',
+      version='0.1',
+      description='A blog for FeinCMS',
+      author='Simon Bächler',
+      author_email='sb@feinheit.ch',
+      url='https://github.com/sbaechler/feincms-elephantblog/',
+      packages=['elephantblog'],
+      )
