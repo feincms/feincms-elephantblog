b13755fac015c773f234c9acc48297f5735ec9d5
Matthias Kestenholz <mk@spinlock.ch>
2011-07-29 16:29:59 +0200

    Add a simple content type showing a list of blog entries

--- elephantblog/contents.py
+++ elephantblog/contents.py
@@ -1,23 +1,26 @@
 #coding=utf-8
 
 from django import forms
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db import models
 from django.template.context import RequestContext
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
+
 from elephantblog.models import Category
+
 
 class BlogGalleryTeaserWidget(models.Model):
     category = models.ForeignKey(Category)
-    
+
     class Meta:
         verbose_name = _('Blog Gallery Teaser Widget')
         verbose_name_plural = _('Blog Gallery Teaser Widgets')
         abstract = True
-    
+
     def __unicode__(self):
         return self.category.__unicode__()
-    
+
     @property
     def media(self):
         media = forms.Media()
@@ -26,7 +29,7 @@ class BlogGalleryTeaserWidget(models.Model):
         media.add_js(('lib/fancybox/jquery.fancybox-1.3.1.pack.js',
                       'content/gallery/gallery.js'))
         return media
-    
+
     def render(self, **kwargs):
         request = kwargs.get('request')
         entries = self.category.blogposts.select_related('gallerycontent', 'richtextcontent')
@@ -43,3 +46,41 @@ class BlogGalleryTeaserWidget(models.Model):
         return render_to_string('blog/widgets/galleryteaserwidget.html',
                                 {'entries': entries },
                                 context_instance=RequestContext(request))
+
+
+class BlogEntryListContent(models.Model):
+    category = models.ForeignKey(Category, blank=True, null=True, related_name='+',
+        verbose_name=_('category'), help_text=_('Only show entries from this category.'))
+    paginate_by = models.PositiveIntegerField(_('paginate by'), default=0,
+        help_text=_('Set to 0 to disable pagination.'))
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def initialize_type(cls, queryset):
+        cls.queryset = queryset
+
+    def process(self, request, **kwargs):
+        entries = self.queryset._clone()
+
+        if self.category:
+            entries = entries.filter(categories=self.category)
+
+        if self.paginate_by:
+            paginator = Paginator(entries, paginate_by=self.paginate_by)
+            page = request.GET.get('page')
+            try:
+                self.entries = paginator.page(page)
+            except PageNotAnInteger:
+                self.entries = paginator.page(1)
+            except EmptyPage:
+                self.entries = paginator.page(paginator.num_pages)
+
+        else:
+            self.entries = entries
+
+    def render(self, **kwargs):
+        return render_to_string('content/blog/entry_list.html', {
+            'content': self,
+            })
