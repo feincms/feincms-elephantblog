fda5e2753df59dff2308aafef07b7bdd78a7d0fe
Matthias Kestenholz <mk@spinlock.ch>
2011-07-29 12:34:39 +0200

    Convert setting variable description to sphinx-discoverable text

--- elephantblog/settings.py
+++ elephantblog/settings.py
@@ -1,26 +1,25 @@
 """
-Default settings for elephantblog. to change these settings, simply set the 
+Default settings for elephantblog. to change these settings, simply set the
 setting in your projects settings.py
 """
+
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
-# How blog comments should be made. Options for now are 'disqus', 
-# 'django-comments' or None
+#: How blog comments should be made. Options for now are 'disqus',
+#: 'django-comments' or None
 BLOG_COMMENTS = getattr(settings, 'BLOG_COMMENTS', None)
 
-# Title of the Blog, currently used for Feed title
+#: Title of the Blog, currently used for Feed title
 BLOG_TITLE = getattr(settings, 'BLOG_TITLE', _('title undefined'))
 
-# Description of the blog, currently used for feed description
-BLOG_DESCRIPTION = getattr(settings, 'BLOG_DESCRIPTION', 
+#: Description of the blog, currently used for feed description
+BLOG_DESCRIPTION = getattr(settings, 'BLOG_DESCRIPTION',
                            _('description undefined'))
 
-# Set pagination for elephantblog.views.entry_list, None to use default from 
-# entry_dict
+#: Set pagination for elephantblog.views.entry_list, None to use default from
+#: entry_dict
 BLOG_LIST_PAGINATION = getattr(settings, 'BLOG_LIST_PAGINATION', None)
 
-# Regions like Page regions:
-
-
+#: Regions like Page regions:
 BLOG_REGIONS = getattr(settings, 'BLOG_REGIONS', (('main', _('Main content area')),))
