fceed7e2781546e576fad604901d761a68c24b09
Matthias Kestenholz <mk@spinlock.ch>
2011-07-29 12:32:59 +0200

    Reorder middleware.py a bit, add a docstring

--- elephantblog/middleware.py
+++ elephantblog/middleware.py
@@ -1,16 +1,22 @@
-from django.utils.translation import activate
+"""
+Middleware which sets the current language based on the start of the path
+
+Add this middleware to ``MIDDLEWARE_CLASSES`` right after the
+``LocaleMiddleware``::
+
+    'django.middleware.locale.LocaleMiddleware',
+    'elephantblog.middleware.SetLocaleFromPath',
+"""
 
 import re
 
+from django.utils.translation import activate
+
 LANGUAGE_RE = re.compile(r'^/(de|en)/')
+
 
 class SetLocaleFromPath(object):
     def process_request(self, request):
         matches = LANGUAGE_RE.match(request.path)
         if matches:
             activate(matches.group(1))
-
-"""
-'django.middleware.locale.LocaleMiddleware',
-'elephantblog.middleware.SetLocaleFromPath', 
-"""
