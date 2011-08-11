fd2c449c095f63fb005cd6a40c8cc7cef885b202
Matthias Kestenholz <mk@spinlock.ch>
2011-08-11 12:29:16 +0200

    Fix obvious problems in the sites extension

--- elephantblog/extensions/sites.py
+++ elephantblog/extensions/sites.py
@@ -8,18 +8,18 @@ from django.db.models import Q
 
 def register(cls, admin_cls):
     cls.add_to_class('sites', ManyToManyField(Site, blank=True,
-        help_text=_('The sites where the blogpost should appear.'), default=Site.objects.get_current()))
+        help_text=_('The sites where the blogpost should appear.'),
+        default=Site.objects.get_current))
 
-    cls.objects.active_filters.update({'sites': Q(sites=Site.objects.get_current())})
+    cls.objects.active_filters.update({'sites': Q(sites=Site.objects.get_current)})
 
-    def sites_admin(self):
-        available_sites = self.sites.all()
+    def sites_admin(self, obj):
+        available_sites = self.obj.all()
         return u', '.join(u'%s' % site.name for site in available_sites)
 
     sites_admin.allow_tags = True
     sites_admin.short_description = _('Sites')
-    cls.sites_admin = sites_admin
 
-    admin_cls.show_on_top.append('sites')
-    admin_cls.list_filter += ('sites',)
+    admin_cls.sites_admin = sites_admin
+    admin_cls.list_filter.append('sites')
     admin_cls.list_display.append('sites_admin')
