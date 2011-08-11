b6bbedfd3e35a1a7a7de221e182f3a8b28c67b93
Stefan Reinhard <sr@feinheit.ch>
2011-08-11 21:07:19 +0200

    reenable tags field

--- elephantblog/extensions/tags.py
+++ elephantblog/extensions/tags.py
@@ -7,6 +7,5 @@ except ImportError:
     raise Exception('django-taggit must be installed to use this extension')
     
 def register(cls, admin_cls):
-    pass
-    #cls.add_to_class('tags', TaggableManager(
-    #    help_text=_('A comma-separated list of tags.')))
+    cls.add_to_class('tags', TaggableManager(
+        help_text=_('A comma-separated list of tags.')))
