175f4a799183c2fea697acaa1fbc9bc34fd1e55a
Matthias Kestenholz <mk@spinlock.ch>
2011-08-11 10:13:51 +0200

    Remove views.py file shadowing the folder

--- elephantblog/views.py
+++
@@ -1,6 +0,0 @@
-import warnings
-
-warnings.warn('Please use elephantblog.views.legacy.views instead of elephantblog.views',
-    DeprecationWarning)
-
-from elephantblog.views.legacy.views import *
