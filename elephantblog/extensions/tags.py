dfa7444ba4889c5c571cc611224ab1a22e7016c3
Stefan Reinhard <sr@feinheit.ch>
2011-08-11 21:04:36 +0200

    simplify tags extension

---
+++ elephantblog/extensions/tags.py
@@ -0,0 +1,12 @@
+
+from django.utils.translation import ugettext_lazy as _
+
+try:
+    from taggit.managers import TaggableManager
+except ImportError:
+    raise Exception('django-taggit must be installed to use this extension')
+    
+def register(cls, admin_cls):
+    pass
+    #cls.add_to_class('tags', TaggableManager(
+    #    help_text=_('A comma-separated list of tags.')))
