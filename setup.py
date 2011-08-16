0cab9a30907009cea8cb4b85b812a04edd1d4e5e
Matthias Kestenholz <mk@spinlock.ch>
2011-08-16 11:50:31 +0200

    setup.py and .gitignore updates

--- setup.py
+++ setup.py
@@ -2,12 +2,19 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+import os
+
+import elephantblog
+
 
 setup(name='feincms-elephantblog',
-      version='0.1',
+      version=elephantblog.__version__,
       description='A blog for FeinCMS',
+      long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read().decode('utf-8'),
       author='Simon Bächler',
       author_email='sb@feinheit.ch',
       url='https://github.com/sbaechler/feincms-elephantblog/',
+      license='BSD License',
+      platforms=['OS Independent'],
       packages=['elephantblog'],
       )
