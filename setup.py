2c3a9e60c0151bb0c90fd0d5800770382b62c023
Matthias Kestenholz <mk@spinlock.ch>
2011-09-21 10:09:17 +0200

    Update MANIFEST.in and setup.py to account for recent changes

--- setup.py
+++ setup.py
@@ -8,13 +8,18 @@ import elephantblog
 
 
 setup(name='feincms-elephantblog',
-      version=elephantblog.__version__,
-      description='A blog for FeinCMS',
-      long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read().decode('utf-8'),
-      author='Simon Bächler',
-      author_email='sb@feinheit.ch',
-      url='https://github.com/sbaechler/feincms-elephantblog/',
-      license='BSD License',
-      platforms=['OS Independent'],
-      packages=['elephantblog'],
-      )
+    version=elephantblog.__version__,
+    description='A blog for FeinCMS',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read().decode('utf-8'),
+    author='Simon Bächler',
+    author_email='sb@feinheit.ch',
+    url='https://github.com/sbaechler/feincms-elephantblog/',
+    license='BSD License',
+    platforms=['OS Independent'],
+    packages=[
+        'elephantblog',
+        'elephantblog.extensions',
+        'elephantblog.management.commands',
+        'elephantblog.templatetags',
+        ],
+    )
