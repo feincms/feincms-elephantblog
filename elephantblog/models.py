db96f5f8e68cc67e80dd2bb7216f6a84975bf2c6
Matthias Kestenholz <mk@spinlock.ch>
2012-02-22 16:56:55 +0100

    Add the add_extension_options helper from the FeinCMS page module

--- elephantblog/models.py
+++ elephantblog/models.py
@@ -154,6 +154,7 @@ class EntryAdmin(item_editor.ItemEditor):
         'slug': ('title',),
         }
 
+    fieldset_insertion_index = 1
     fieldsets = [
         [None, {
             'fields': [
@@ -165,3 +166,12 @@ class EntryAdmin(item_editor.ItemEditor):
         }],
         item_editor.FEINCMS_CONTENT_FIELDSET,
     ]
+
+    @classmethod
+    def add_extension_options(cls, *f):
+        if isinstance(f[-1], dict):     # called with a fieldset
+            cls.fieldsets.insert(cls.fieldset_insertion_index, f)
+            f[1]['classes'] = list(f[1].get('classes', []))
+            f[1]['classes'].append('collapse')
+        else:   # assume called with "other" fields
+            cls.fieldsets[0][1]['fields'].extend(f)
