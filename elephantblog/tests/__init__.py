f4620aff41c4cb9d341f7f67e06480faf860a41a
Simon Bächler <simon@stellanera.com>
2012-10-14 21:44:06 +0200

    add comment on how to run the tests

--- elephantblog/tests/__init__.py
+++ elephantblog/tests/__init__.py
@@ -1,5 +1,12 @@
+"""
+To use the test suite you can use the included test project. It's a very basic
+django app that is fully functional.
 
-# ./elephantblog/tests/testapp/manage.py test --failfast elephantblog
+1. Create a virtualenv
+2. run ``pip install -r requirements_dev.txt to install the testing tools``
+3. run the tests: ``./elephantblog/tests/testapp/manage.py test  elephantblog``
+"""
+
 from .test_translations import *
 from .test_timezones import *
 from .test_archive_views import *
