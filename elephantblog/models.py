7983af6488607385d3135923e046131de0f64948
George Marshall <echosx@gmail.com>
2013-01-22 10:32:04 -0800

    Save method now indicates it changes the data

--- elephantblog/models.py
+++ elephantblog/models.py
@@ -118,6 +118,7 @@ class Entry(Base):
             self.published_on = now()
 
         super(Entry, self).save(*args, **kwargs)
+    save.alters_data = True
 
     @models.permalink
     def get_absolute_url(self):
