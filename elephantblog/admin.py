75bbdaebd31a1ed3c134f7b13b0315a540cd8259
Matthias Kestenholz <mk@spinlock.ch>
2013-02-26 10:17:59 +0100

    Remove an unneeded import

--- elephantblog/admin.py
+++ elephantblog/admin.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from elephantblog import models
 
-from feincms.translations import admin_translationinline, short_language_code
+from feincms.translations import admin_translationinline
 
 
 CategoryTranslationInline = admin_translationinline(
