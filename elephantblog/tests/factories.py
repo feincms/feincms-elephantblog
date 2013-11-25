3d0417eb17c4735eed8774dce4c8e065fce64005
Matthias Kestenholz <mk@spinlock.ch>
2013-11-25 17:26:57 +0100

    flake8-clean elephantblog/tests/factories.py

--- elephantblog/tests/factories.py
+++ elephantblog/tests/factories.py
@@ -8,15 +8,16 @@ from django.contrib.auth.models import User
 
 class UserFactory(factory.DjangoModelFactory):
     FACTORY_FOR = User
-    username='author'
-    password='elephant'
-    email='admin@elephantblog.ch'
+    username = 'author'
+    password = 'elephant'
+    email = 'admin@elephantblog.ch'
 
 
 class EntryFactory(factory.DjangoModelFactory):
     FACTORY_FOR = Entry
-    is_active=True
-    is_featured=False
+    is_active = True
+    is_featured = False
+
 
 def create_entries(factory):
     author = UserFactory()
@@ -25,16 +26,16 @@ def create_entries(factory):
         pk=1,
         author=author,
         title=u'Entry 1',
-        published_on=datetime.datetime(2012,8,12, 11,0,0),
-        last_changed=datetime.datetime(2012,8,12, 15,0,0),
+        published_on=datetime.datetime(2012, 8, 12, 11, 0, 0),
+        last_changed=datetime.datetime(2012, 8, 12, 15, 0, 0),
         slug='entry-1'
     ))
     entries.append(factory.create(
         pk=2,
         author=author,
         title=u'Eintrag 1',
-        published_on=datetime.datetime(2012,10,12, 11,0,0),
-        last_changed=datetime.datetime(2012,10,12, 15,0,0),
+        published_on=datetime.datetime(2012, 10, 12, 11, 0, 0),
+        last_changed=datetime.datetime(2012, 10, 12, 15, 0, 0),
         slug='eintrag-1'
     ))
     return entries
@@ -49,8 +50,8 @@ def create_chinese_entries(factory):
         title=u'Entry 2 chinese traditional',
         language='zh-cn',
         translation_of=entries[0],
-        published_on=datetime.datetime(2012,10,12, 12,0,0),
-        last_changed=datetime.datetime(2012,10,12, 16,0,0),
+        published_on=datetime.datetime(2012, 10, 12, 12, 0, 0),
+        last_changed=datetime.datetime(2012, 10, 12, 16, 0, 0),
         slug='entry-2-cn'
     )
     factory.create(
@@ -59,26 +60,25 @@ def create_chinese_entries(factory):
         title=u'Entry 2 chinese simplified',
         language='zh-tw',
         translation_of=entries[0],
-        published_on=datetime.datetime(2012,10,12, 12,0,0),
-        last_changed=datetime.datetime(2012,10,12, 16,0,0),
+        published_on=datetime.datetime(2012, 10, 12, 12, 0, 0),
+        last_changed=datetime.datetime(2012, 10, 12, 16, 0, 0),
         slug='entry-2-tw'
     )
-
-
 
 
 class CategoryTranslationFactory(factory.DjangoModelFactory):
     FACTORY_FOR = CategoryTranslation
 
+
 class CategoryFactory(factory.DjangoModelFactory):
     FACTORY_FOR = Category
 
+
 def create_category(title):
     category = CategoryFactory.create()
-    translation = CategoryTranslationFactory.create(
-        parent = category,
-        title = title,
-        slug = slugify(title)
+    CategoryTranslationFactory.create(
+        parent=category,
+        title=title,
+        slug=slugify(title)
     )
     return category
-
