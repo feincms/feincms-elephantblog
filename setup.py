588be07b49b27aceb597766f80ac105518f50570
Matthias Kestenholz <mk@spinlock.ch>
2014-01-06 11:31:20 +0100

    Actually, only FeinCMS 1.9.2 is compatible with Python 3.2

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development',
