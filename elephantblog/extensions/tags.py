e1f49aa14cbdd6f12a2a2141261ca5d684443db9
Simon Baechler <sb@feinheit.ch>
2014-08-07 14:45:59 +0200

    update tag extension

--- elephantblog/extensions/tags.py
+++ elephantblog/extensions/tags.py
@@ -3,12 +3,15 @@ from __future__ import absolute_import, unicode_literals
 from django.utils.translation import ugettext_lazy as _
 
 from taggit.managers import TaggableManager
+from feincms.extensions import Extension as FeincmsExtension
 
 
-def register(cls, admin_cls):
-    cls.add_to_class('tags', TaggableManager(
-        help_text=_('A comma-separated list of tags.'), blank=True))
+class Extension(FeincmsExtension):
 
-    if admin_cls:
-        if hasattr(admin_cls, 'add_extension_options'):
-            admin_cls.add_extension_options('tags')
+    def handle_model(self):
+        self.model.add_to_class('tags', TaggableManager(
+            help_text=_('A comma-separated list of tags.'), blank=True))
+
+    def handle_modeladmin(self, modeladmin):
+        if hasattr(modeladmin, 'add_extension_options'):
+            modeladmin.add_extension_options('tags')
