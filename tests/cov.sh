0e76c61150b4086b35f3ef1dbd8eee1d7412b6c4
Matthias Kestenholz <mk@spinlock.ch>
2015-09-02 17:13:17 +0200

    Oops

--- tests/cov.sh
+++ tests/cov.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-coverage run --branch --include="*elephantblog/elephantblog*" ./manage.py test testapp
+coverage run --branch --include="*elephantblog/elephantblog*" ./manage.py test testapp -v 2
 coverage html
