db992d5704131a72b9c872c59faa9dc07c1b39f5
Matthias Kestenholz <mk@spinlock.ch>
2015-09-02 20:12:18 +0200

    Discovery test runner rocks

--- tests/testapp/tests/__init__.py
+++ tests/testapp/tests/__init__.py
@@ -6,14 +6,3 @@ django app that is fully functional.
 2. run ``pip install -r requirements.txt to install the testing tools``
 3. run the tests: ``manage.py test testapp``
 """
-
-from __future__ import absolute_import, unicode_literals
-
-from .test_translations import *  # noqa
-from .test_timezones import *  # noqa
-from .test_archive_views import *  # noqa
-from .test_templatetags import *  # noqa
-from .test_feed import *  # noqa
-from .test_contents import *  # noqa
-from .test_admin import *  # noqa
-from .test_sitemap import *  # noqa
