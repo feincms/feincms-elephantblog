d97a971daa9e3faed80ccf092383f6bd040724d6
Nils Fredrik Gjerull <nils@gjerull.net>
2017-12-28 16:53:38 +0100

    Make compatible with Django 2.0.

--- tests/testapp/tests/test_admin.py
+++ tests/testapp/tests/test_admin.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 
-from django.core.urlresolvers import reverse
+try:
+    from django.urls import reverse
+except ImportError:
+    from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 from elephantblog.models import Entry
