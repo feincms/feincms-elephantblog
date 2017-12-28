5e552bcd672680e0756a5f085c78c08a8a9632a2
Nils Fredrik Gjerull <nils@gjerull.net>
2017-12-28 18:46:12 +0100

    Django 2.0 need Python 3.4 or newer.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     ),
     include_package_data=True,
     install_requires=[
-        'Django>=1.8',
+        'Django>=1.8',  # For Django 2.0 you need Python 3.4 or newer.
         'FeinCMS>=1.11',
     ],
     classifiers=[
