f1034311593563b36bd2725efaa722bd3be14c0c
Matthias Kestenholz <mk@feinheit.ch>
2022-01-07 17:02:54 +0100

    Declarative setup, Github actions, etc.

--- setup.py
+++ setup.py
@@ -1,42 +1,5 @@
 #!/usr/bin/env python3
-
-import os
-
-from setuptools import find_packages, setup
-
-import elephantblog
+from setuptools import setup
 
 
-setup(
-    name="feincms-elephantblog",
-    version=elephantblog.__version__,
-    description="A blog for FeinCMS",
-    long_description=open(os.path.join(os.path.dirname(__file__), "README.rst")).read(),
-    author="Simon Baechler",
-    author_email="sb@feinheit.ch",
-    url="https://github.com/feincms/feincms-elephantblog/",
-    license="BSD License",
-    platforms=["OS Independent"],
-    packages=find_packages(
-        exclude=[],
-    ),
-    include_package_data=True,
-    install_requires=["Django>=3.2", "FeinCMS>=22.0"],
-    python_requires=">=3.9",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        "Topic :: Software Development",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
-    ],
-    zip_safe=False,
-)
+setup()
